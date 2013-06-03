8644f03f3edd4753bfd07b9396f018c30e627add
robertSahm <robertsahm@gmail.com>
2013-06-03 18:00:59 -0400

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sahm::Application.config.secret_key_base = '31193dc05e554df500aa9af228ed88c748ed5c15beadf248827875a98dafa32151865f7281ea296e45a87ed439d35439e8d57208d71cf83df22e2f18a4f32d46'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sahm::Application.config.session_store :cookie_store, key: '_sahm_session'

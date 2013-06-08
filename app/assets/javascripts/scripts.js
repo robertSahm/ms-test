11fe97881fafea0d1bd18a197acfb506a504e5fb
robertSahm <robertsahm@gmail.com>
2013-06-07 20:48:19 -0400

    flippy4lyfe

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,120 +1,158 @@
-// stroll.bind( '#main ul' );
-
-// $(document).ready(function() {
-//     var scrollorama = $.scrollorama({
-//         blocks:'.scrollblock'
-//     });
-// });
-
-
-
-
-// $(function () {
-//   // Utilize the modernzr feature support class to detect CSS 3D transform support
-//   if ($('html').hasClass('csstransforms3d')) {    
-        
-//     // if it's supported, remove the scroll effect add the cool card flipping instead
-//     $('.thumb').removeClass('scroll').addClass('flip');        
-            
-//       // add/remove flip class that make the transition effect
-//     $('.thumb.flip').hover (
-//         function () {
-//             $(this).find('.thumb-wrapper').addClass('flipIt flip');
-//         },
-//         function () {
-//             $(this).find('.thumb-wrapper').removeClass('flipIt');            
-//         }
-//       );
-            
-//     } else {
-        
-//       // CSS 3D is not supported, use the scroll up effect instead
-//       $('.thumb').hover(
-//           function () {
-//               $(this).find('.thumb-detail').stop().animate({bottom:0}, 500, 'easeOutCubic');
-//           },
-//           function () {
-//               $(this).find('.thumb-detail').stop().animate({bottom: ($(this).height() * -1) }, 500, 'easeOutCubic');            
-//           }
-//       );
-//     }
-//   });
-
-// $(document).ready(function() {
-//     $("#myFlippyBox").click(function() {
-
-//         $(this).flippy({
-//             //content : "Hi !",
-//             verso: "<img src=\"http://placekitten.com/610/300\" alt=\"placeholder\"/>",
-//             recto : "Hi! Bitches",
-//             //verso: "<%= image_tag "portaBlock.png" %>",
-//             direction : "TOP",
-//             duration : "750",
-//             // onStart : function() {
-                
-//             // },
-//             onFinish : function() {
-//                $(this).flippy({
-//                   verso: "Hi!"
-//                 });
-//             }
-//         });
-
-//     });
-// });
-
-
-// $(".flipbox").on("click",function(e){
-//   $(".flipbox").flippy({
-//     color_target: "red",
-//     duration: "500",
-//     verso: "<div class='flipOver' style='background-image: url('portaLogo.png');'></div>",
-//     //verso: "<img src=\"http://placekitten.com/610/300\" alt=\"placeholder\"/>",
-//     //recto : "<img src='images/portaLogo.png'>",
-//     onStart: function(){
-//       // Your code here
-    
-
-
-//     }
-//   });
-//   //e.preventDefault();
-// });
-
-
-function flip(t) {      // t is the id/class of the element
-    $(t).off().flippy({ // here instead of t you can also write #myFlippyBox and remove it
-                        // .off() removes mouseout/mouseover handlers
-        color_target: "#a7a7a7",
-        verso: "<p class='title'>This is a stock image</p>",
+ 
+// Porta Flipper 
+$(function () {
+  $('#portaBlockWrapper').each(function(){
+    function flip(t) {      // t is the id/class of the element
+      $('#portaClick').off().flippy({   // here instead of t you can also write #portaClick and remove it
+                            // .off() removes mouseout/mouseover handlers
+        color_target: "none",
+        depth : "0",
+        light : ".1",
+        direction : "TOP",
+        duration : "400",
+        verso: "<img class='small-12 small-centered' src='/assets/portaBlock.png' class='afterBlock' />",
+        //recto: "<img class='small-12 small-centered' src='/assets/ketchBlock.png' class='afterBlock' />",
         onFinish: function () {
-            $('#myFlippyBox').off().on('click', function () { 
-                                           // remove handles again and add mouseout
-                flipBack('#myFlippyBox');
-            });
+
+          $('#portaClick').off().on('click', function () { 
+            $('#portaClick').addClass('afterBlock');
+
+            // remove(d) handles again and add mouseout
+            flipBack('#portaClick');
+          });
         },
         onReverseFinish: function () {
-            $('#myFlippyBox').off().on('click', function(){
-              flip('#myFlippyBox');
-            });
+          $('#portaClick').off().on('click', function(){
+            flip('#portaClick');
+          });
         }
+      });
+    }
+    
+    function flipBack(t) {
+      $(t).flippyReverse({
+        direction: "TOP",
+        color_target: "none",
+        verso: "<img class='small-12 small-centered' src='/assets/portaBlock.png' alt='Portablock' />",
+      });
+    }
+    
+    $(function () {
+      $("#portaClick").on('click', function () { 
+        // at start bind mouseover and run function flip()
+        flip('#portaClick'); // here you send the id/class of the element
+      });
     });
-}
+  });
+});
 
-function flipBack(t) {
-    $(t).flippyReverse({
-        color_target: "#a7a7a7 ",
-        recto: "<img src='http://static.blazonco.com/customcss/dyllen/HayworthCreative/images/ceo.jpeg'/>"
+
+//Ketch Flipper
+$(function () {
+  $('#ketchBlockWrapper').each(function(){
+    function flip(t) {      // t is the id/class of the element
+      $('#ketchClick').off().flippy({   // here instead of t you can also write #portaClick and remove it
+                            // .off() removes mouseout/mouseover handlers
+        color_target: "none",
+        depth : "0",
+        light : ".1",
+        direction : "TOP",
+        duration : "400",
+        verso: "<img class='small-12 small-centered' src='/assets/ketchBlock.png' class='afterBlock' />",
+        //recto: "<img class='small-12 small-centered' src='/assets/ketchBlock.png' class='afterBlock' />",
+        onFinish: function () {
+
+          $('#ketchClick').off().on('click', function () { 
+            $('#ketchClick').addClass('afterBlock');
+
+            // remove(d) handles again and add mouseout
+            flipBack('#ketchClick');
+          });
+        },
+        onReverseFinish: function () {
+          $('#ketchClick').off().on('click', function(){
+            flip('#ketchClick');
+          });
+        }
+      });
+    }
+    
+    function flipBack(t) {
+      $(t).flippyReverse({
+        direction: "TOP",
+        color_target: "none",
+        verso: "<img class='small-12 small-centered' src='/assets/portaBlock.png' alt='Portablock' />",
+      });
+    }
+    
+    $(function () {
+      $("#ketchClick").on('click', function () { 
+        // at start bind mouseover and run function flip()
+        flip('#ketchClick'); // here you send the id/class of the element
+      });
     });
-}
+  });
+});
 
+//C2C Flipper
 
 $(function () {
-    $("#myFlippyBox").on('click', function () { 
+  $('#c2cBlockWrapper').each(function(){
+    function flip(t) {      // t is the id/class of the element
+      $('#c2cClick').off().flippy({   // here instead of t you can also write #portaClick and remove it
+                            // .off() removes mouseout/mouseover handlers
+        color_target: "none",
+        depth : "0",
+        light : ".1",
+        direction : "TOP",
+        duration : "400",
+        verso: "<img class='small-12 small-centered' src='/assets/c2cBlock.png' class='afterBlock' />",
+        //recto: "<img class='small-12 small-centered' src='/assets/ketchBlock.png' class='afterBlock' />",
+        onFinish: function () {
+
+          $('#c2cClick').off().on('click', function () { 
+            $('#c2cClick').addClass('afterBlock');
+
+            // remove(d) handles again and add mouseout
+            flipBack('#c2cClick');
+          });
+        },
+        onReverseFinish: function () {
+          $('#c2c').off().on('click', function(){
+            flip('#c2cClick');
+          });
+        }
+      });
+    }
+    
+    function flipBack(t) {
+      $(t).flippyReverse({
+        direction: "TOP",
+        color_target: "none",
+        verso: "<img class='small-12 small-centered' src='/assets/c2cBlock.png' alt='Portablock' />",
+      });
+    }
+    
+    $(function () {
+      $("#c2cClick").on('click', function () { 
         // at start bind mouseover and run function flip()
-        flip('#myFlippyBox'); // here you send the id/class of the element
+      flip('#c2cClick'); // here you send the id/class of the element
+      });
     });
+  });
 });
+// $('.box').click(function () {
+//   //$('.')
+//   $('.box').transition({
+//     perspective: '100px',
+//     rotateX: '180deg'
+//   });
+// });
+
+
+
+
+
 
 
 

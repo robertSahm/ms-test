bb7258d44f52a4bee6cf0171ee3a3036b528934a
robertSahm <robertsahm@gmail.com>
2013-06-15 19:48:47 -0400

    adding lots of images for deflate gem

--- Gemfile
+++ Gemfile
@@ -21,6 +21,8 @@ gem 'coffee-rails', '~> 4.0.0'
 # See https://github.com/sstephenson/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
 
+gem 'heroku_rails_deflate', :group => :production
+
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ Sahm::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
@@ -34,6 +34,10 @@ Sahm::Application.configure do
 
   # Version of your assets, change this if you want to expire all your assets.
   config.assets.version = '1.0'
+
+
+  #max-age for your static assets
+  config.static_cache_control = "public, max-age=31536000"
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache

5ef7e5a41a01311b43a07969caa04ebda176a363
robertSahm <robertsahm@gmail.com>
2013-06-17 15:53:34 -0400

    trying...

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ Sahm::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = true
+  config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
@@ -37,7 +37,7 @@ Sahm::Application.configure do
 
 
   #max-age for your static assets
-  config.static_cache_control = "public, max-age=31536000"
+  # config.static_cache_control = "public, max-age=31536000"
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
